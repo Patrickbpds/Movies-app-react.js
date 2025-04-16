6d11fed71bd6ab620d611ebd6fecd5f5d48304f6
Patrick <patrickbpds@gmail.com>
2025-04-16 20:27:56 -0300

    feat: favorite page upgrade

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -2,23 +2,36 @@ import { useContext } from "react";
 import { FavoritesContext } from "../context/FavoritesContext";
 
 const FavoritesPage = () => {
-  const { favorites } = useContext(FavoritesContext);
+  const { favorites, removeFavorite } = useContext(FavoritesContext);
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">My Favorite Movies</h1>
+    <div className="p-4 rounded-xl shadow-lg bg-gradient-to-r from-[#080a3d] to-[#241940]">
+      <h1 className="text-2xl font-bold text-white mb-4">My Favorite Movies</h1>
       {favorites.length === 0 ? (
-        <p>You have not added any favorites yet.</p>
+        <p className="text-white">You have not added any favorites yet.</p>
       ) : (
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           {favorites.map((movie) => (
-            <div key={movie.id} className="border p-2 rounded shadow">
-              <h2 className="font-medium">{movie.title}</h2>
-              <img
-                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                alt={movie.title}
-                className="rounded"
-              />
+            <div
+              key={movie.id}
+              className="p-4 rounded-xl shadow-lg bg-gradient-to-r from-[#080a3d] to-[#241940]"
+            >
+              <h2 className="font-bold text-xl text-white mb-3 truncate text-center">
+                {movie.title}
+              </h2>
+              <div className="w-full flex justify-center mb-3">
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  alt={movie.title}
+                  className="rounded-lg shadow-md"
+                />
+              </div>
+              <button
+                onClick={() => removeFavorite(movie.id)}
+                className="w-full flex items-center justify-center mt-2 px-4 py-2 bg-red-500 text-white font-bold rounded hover:bg-red-600"
+              >
+                Remove from Favorites
+              </button>
             </div>
           ))}
         </div>
